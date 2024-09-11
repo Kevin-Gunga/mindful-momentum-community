b3b1f864fd4d75002356eab95d2537f4354c805e
kevin <kevin@kali.kevin>
2024-09-11 09:24:00 +0300

    added models,superuser

--- myenv/myproject/myapp/models.py
+++ myenv/myproject/myapp/models.py
@@ -1,3 +1,13 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    email = models.EmailField(max_length=254, null=False)
+    phone_number = models.CharField(max_length=15, null=False)
+    location = models.CharField(max_length=254, null=False)
+    submitted_at = models.TimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'{self.phone_number} {self.location}'
+    
--- myenv/myproject/myapp/views.py
+++ myenv/myproject/myapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Booking
 
 # Create your views here.
 
-def index (request):         
+def index (request): 
+    if request  == 'POST':
+        email = request.POST.get('email')   
+        phone = request.POST.get('phone') 
+        location = request.POST.get('location')
+        
+        book = Booking(email = email, phone_number = phone, location = location) 
+        book.save()   
+            
     return render(request, 'myapp/index.html') 
